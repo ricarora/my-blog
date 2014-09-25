6cb9a18e1b335257dbd7d2ab58c7539e51804724
Richa <ricarora@gmail.com>
2014-09-25 10:31:30 -0700

    class Post commit

--- lib/post.rb
+++ lib/post.rb
@@ -5,13 +5,16 @@ class Post
   # Read my list of post files
   # Create an array of Post objects with the attributes set
   # Post.all => [post, post]
+attr_accessor :title, :url
 
   def initialize(array)
-    
+    @title = array[-1].sub(".erb", "")
+    @url = "/posts/#{@title}"
+
   end
 
   def self.all
-    Dir.glob("views/posts/*.erb").each do |filepath|
+    Dir.glob("views/posts/*.erb").collect do |filepath|
       a = filepath.split("/")
       self.new(a)
     end
